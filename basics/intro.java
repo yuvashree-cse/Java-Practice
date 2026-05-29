e0cdc1fa9e26480312a89d1cc5bc09db7d9d1012
Yuvashree Chandra Sekar <yuvashreecv@gmail.com>
2026-05-29 17:20:30 +0530

    day 1 continuation

---
+++ basics/intro.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class intro{
+
+    public static void main(String[] args){
+        System.out.println("Hi, I am Yuvashree!");
+        System.out.println("I am from Chennai!");
+    }
+}
