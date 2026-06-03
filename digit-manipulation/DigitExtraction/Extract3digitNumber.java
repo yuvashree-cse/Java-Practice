c50e96702c9d0cc77c7528ea7f826f9c7187dd5e
Yuvashree Chandra Sekar <yuvashreecv@gmail.com>
2026-06-03 18:28:55 +0530

    day 5

---
+++ digit-manipulation/DigitExtraction/Extract3digitNumber.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Extract3digitNumber{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nExtracting 3 digit number");
+        System.out.print("\nEnter any 3 digit number: ");
+        int num = sc.nextInt();
+        int last = num % 10;
+        int middle = (num / 10) % 10;
+        int first = num / 100;
+
+        System.out.println("\nExtracted digits: ");
+        System.out.print("First Digit: "+first+",\n");
+        System.out.print("Middle Digit: "+middle+",\n");
+        System.out.print("Last Digit: "+last);
+    }
+}
