11036a316a54c0c9b44cb89842969ac29549363d
Yuvashree Chandra Sekar <yuvashreecv@gmail.com>
2026-07-11 22:20:51 +0530

    day 15 continuation

---
+++ loops/AccumulatorVariables/SumFrom1To10.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class SumFrom1To10{
+    public static void main(String[] args){
+        int sum = 0;
+        for(int i = 1; i <= 10; i++){
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
